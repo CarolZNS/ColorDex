14e9ecaea34af7695ec97d34c18e07826467f448
CarolZNS <nunes.carolzs@gmail.com>
2022-11-25 18:14:15 -0300

    Altera a ordem dos status

--- src/components/StatusList/styles.js
+++ src/components/StatusList/styles.js
@@ -4,6 +4,12 @@ export const InnerGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 0.4rem;
+  &:nth-of-type(2){
+    grid-row-start: 2;
+  }
+  &:nth-of-type(3){
+    grid-row-start: 3;
+  }
 `;
 
 export const Name = styled.span`
